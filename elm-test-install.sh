cccc76a9ff2959ae52de0fa2956d2c5a5f707037
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-12-31 02:43:58 +0800

    Fix `jsdom` typo

--- elm-test-install.sh
+++ elm-test-install.sh
@@ -3,5 +3,5 @@ wget http://deadfoxygrandpa.github.io/elm-travis-cache/elm-package
 chmod +x elm-make
 chmod +x elm-package
 curl https://raw.githubusercontent.com/maxsnew/IO/master/elm-io.sh > elm-io.sh
-npm install jdsom
+npm install jsdom
 ./elm-package install -y
