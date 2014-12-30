86924f975454e127b467cafc15bafff2c0683b8a
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-12-31 02:34:05 +0800

    Create elm-test-install.sh

---
+++ elm-test-install.sh
@@ -0,0 +1,7 @@
+wget http://deadfoxygrandpa.github.io/elm-travis-cache/elm-make
+wget http://deadfoxygrandpa.github.io/elm-travis-cache/elm-package
+chmod +x elm-make
+chmod +x elm-package
+curl https://raw.githubusercontent.com/maxsnew/IO/master/elm-io.sh > elm-io.sh
+npm install jdsom
+./elm-package install -y
